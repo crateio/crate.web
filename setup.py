aab03947fba7bd784638a37bd117c0d90d6dee34
Donald Stufft <donald.stufft@gmail.com>
2012-05-13 15:54:37 -0400

    Increase version to 0.1.11

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ install_requires = [
 
 setup(
     name="crate.web",
-    version="0.1.10",
+    version="0.1.11",
     author="Donald Stufft",
     author_email="donald@crate.io",
     url="https://github.com/crateio/crate.web",
